55f50c35efb3bec6873706ba4c77af6d89aa9b14
“maribellamoreno” <maribellavmoreno@gmail.com>
2025-07-02 12:05:47 -0400

    prueba enviar formulario vacio

--- cypress/e2e/validar.formulario.spec.cy.js
+++ cypress/e2e/validar.formulario.spec.cy.js
@@ -7,6 +7,17 @@ describe("Acceso al site y envio de formulario ", () => {
     cy.get('[name="email"]').type('correo'); //falla correo no valido
     cy.get('#message').type('Mensaje');
     cy.get('#enviar_formulario').click();
-    cy.get('.contactenos').contains('Formulario enviado!'); //usando metodo contains
+    //cy.get('.contactenos').contains('Formulario enviado!');
+    cy.get('.contactenos').should('be.visible'); //usando metodo contains
+    });
+
+    it("Enviar formulario vacio", () => {
+    //cuerpo o instrucciones o acciones de la prueba
+    cy.visit('https://ljcl79.github.io/primera-pagina-ia-taller-adl/'); //Cypress visita esta web
+    cy.get('[name="name"]').type('maribella');
+    cy.get('[name="email"]').clear(); //usando metodo clear
+    cy.get('#message').clear();
+    cy.get('#enviar_formulario').click(); //usando metodo click
+    cy.get('#enviar_formulario').should('be.visible'); //usando metodo should
     });
 });
